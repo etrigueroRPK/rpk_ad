9d935fac679b34d252f0e77393e64d1704c097a6
edmundo <edmundotriguero@gmail.com>
2019-12-18 00:50:13 -0400

    creacion de app content

---
+++ content/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+from bases.models import ClassModel
+from sales.models import Contract
+# Create your models here.
+ 
+
+class Video(ClassModel):
+    name = models.CharField(max_length=100)
+    duration = models.TimeField()
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    description = models.CharField(null=True, blank=True)
+    img = models.ImageField(upload_to='videos', null=True, blank=True)
+    video = models.FileField(upload_to='videos', null=True, blank=True)
+    contract = models.ForeignKey(Contract, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{}:{}'.format(self.name, self.duration)
+
