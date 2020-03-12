23e87f398f7e41f1e760ea01e29b59967130f117
Edmundo_linux <edmundotriguero@gmail.com>
2020-03-12 13:12:14 -0400

    playlist avanzando

--- content/urls.py
+++ content/urls.py
@@ -15,6 +15,8 @@ urlpatterns = [
     path('playlist/', playlist_list, name='playlist_list'),
     path('playlist/generator', playlist_generator, name='playlist_generator'),
     path('playlist/contract_list/', playlist_generator, name='playlist_contract_list'),
+
+    path('playlist/generator/generate', playlist_generator, name='playlist_generate'),
     
 
 
