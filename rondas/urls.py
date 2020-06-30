641bceb2293ca007f10fbd32826c718a797f32db
edmundo <edmundotriguero@gmail.com>
2020-06-30 01:57:29 -0400

    Se realizo el uso de mapas para mostrar la ubicacion de latutud y longitud en rondas

--- rondas/urls.py
+++ rondas/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
-from .views import RondasView, RondaNew
+from .views import RondasView, RondaNew, inplace_view, inplace_delete
 # from .views import VideoView, VideoNew, VideoEdit, VideoForm, video_delete, video_disabled, \
     # playlist_list, playlist_generator, playlist_order, playlist_new, playlist_view, playlist_delete
 
 urlpatterns = [
     # vistas para registro de rondas
     path('rondas/', RondasView.as_view(), name='rondas_list'),
-    path('videos/new', RondaNew.as_view(), name='ronda_new'),
+    path('ronda/new', RondaNew.as_view(), name='ronda_new'),
+    path('ronda/view/<int:id>', inplace_view, name='ronda_view'),
+
     # path('videos/edit/<int:pk>',
     #      VideoEdit.as_view(), name='video_edit'),
     # path('videos/disabled/<int:id>', video_disabled, name='video_disabled'),
-    # path('videos/delete/<int:id>', video_delete, name='video_delete'),
+    path('ronda/delete/<int:id>', inplace_delete, name='ronda_delete'),
 
 ]
--- rondas/views.py
+++ rondas/views.py
@@ -54,3 +54,52 @@ class RondaNew(SuccessMessageMixin, LoginRequiredMixin, generic.CreateView):
         form.instance.user_created = self.request.user
 
         return super().form_valid(form)
+
+
+@login_required(login_url='/login/')
+@permission_required('rondas.view_inplace', login_url='bases:sin_privilegios')
+def inplace_view(request, id):
+    template_name = 'rondas/inplace_view.html'
+    contexto = {}
+    cat = Inplace.objects.filter(pk=id).first()
+
+    if not cat:
+        return HttpResponse('Registro no encontrado' + str(id))
+
+    if request.method == 'GET':
+        contexto = {'obj': cat}
+
+    if request.method == 'POST':
+        # cat.state = False
+        cat.update()
+        # mensaje par que la vista lo muestre sin coloca en comentarios pues al momento de los esta haciendo con ajax
+        # messages.success(request, 'Se inactivo correctamente')
+
+        contexto = {'obj': 'OK'}
+        return HttpResponse('Registro  visto')
+
+    return render(request, template_name, contexto)
+
+@login_required(login_url='/login/')
+@permission_required('rondas.delete_inplace', login_url='bases:sin_privilegios')
+def inplace_delete(request, id):
+    template_name = 'rondas/inplace_delete.html'
+    contexto = {}
+    cat = Inplace.objects.filter(pk=id).first()
+
+    if not cat:
+        return HttpResponse('Registro no encontrado' + str(id))
+
+    if request.method == 'GET':
+        contexto = {'obj': cat}
+
+    if request.method == 'POST':
+        # cat.state = False
+        cat.delete()
+        # mensaje par que la vista lo muestre sin coloca en comentarios pues al momento de los esta haciendo con ajax
+        # messages.success(request, 'Se inactivo correctamente')
+
+        contexto = {'obj': 'OK'}
+        return HttpResponse('Registro  eliminado')
+
+    return render(request, template_name, contexto)
